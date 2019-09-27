3ea2f30c933e144262f73a975d9da8be73cf01eb
melos-c <55641771+melos-c@users.noreply.github.com>
2019-09-27 10:08:44 +0800

    Update installv2ray.sh

--- installv2ray.sh
+++ installv2ray.sh
@@ -13,7 +13,7 @@ cp v2ray /usr/bin/v2ray/v2ray
 cp v2ctl /usr/bin/v2ray/v2ctl
 cp geoip.dat /usr/bin/v2ray/geoip.dat
 cp geosite.dat /usr/bin/v2ray/geosite.dat
-cp vpoint_vmess_freedom.json /etc/v2ray/config.json
+#cp vpoint_vmess_freedom.json /etc/v2ray/config.json
 cp ./systemd/v2ray.service /usr/lib/systemd/system
 
 touch /var/log/v2ray/access.log
@@ -22,7 +22,8 @@ touch /var/run/v2ray.pid
 cd ..
 
 cd /etc/v2ray/
-wget -O config.json https://github.com/melos-c/VtR/blob/master/config-server.json
+wget -O config.json https://raw.githubusercontent.com/melos-c/VtR/master/config-server.json
+cd ~
 
 systemctl start v2ray
 systemctl status v2ray
@@ -30,3 +31,11 @@ systemctl enable v2ray
 
 firewall-cmd --zone=public --add-port=10086/tcp --permanent
 firewall-cmd --reload
+
+wget –no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+chmod +x bbr.sh
+./bbr.sh
+reboot
+
+# Verify if BBR is installed successfully.
+#lsmod | grep bbr
