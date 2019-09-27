1e7ee6e995b2147e3d3b1d7ae6283501fdeec4de
melos-c <55641771+melos-c@users.noreply.github.com>
2019-09-27 09:36:25 +0800

    Update installv2ray.sh

--- installv2ray.sh
+++ installv2ray.sh
@@ -15,9 +15,11 @@ cp geoip.dat /usr/bin/v2ray/geoip.dat
 cp geosite.dat /usr/bin/v2ray/geosite.dat
 cp vpoint_vmess_freedom.json /etc/v2ray/config.json
 cp ./systemd/v2ray.service /usr/lib/systemd/system
+
 touch /var/log/v2ray/access.log
 touch /var/log/v2ray/error.log
 touch /var/run/v2ray.pid
+
 systemctl start v2ray
 systemctl status v2ray
 systemctl enable v2ray
