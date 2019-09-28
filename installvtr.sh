4beb4725e11243b2e2d0d83a42ac160fc94c748f
melos-c <55641771+melos-c@users.noreply.github.com>
2019-09-28 09:42:53 +0800

    Update installvtr.sh

--- installvtr.sh
+++ installvtr.sh
@@ -1,4 +1,5 @@
 yum install -y zip unzip
+#yum install net-tools
 wget https://github.com/v2ray/v2ray-core/releases/download/v4.20.0/v2ray-linux-64.zip
 
 mkdir v2ray
@@ -29,10 +30,15 @@ systemctl start v2ray
 systemctl status v2ray
 systemctl enable v2ray
 
-#firewall-cmd --zone=public --add-port=10086/tcp --permanent
-firewall-cmd --zone=public --add-port=20000-30000/tcp --permanent
+echo "---------------------开放防火墙端口---------------------"
+firewall-cmd --zone=public --add-port=10086/tcp --permanent
+firewall-cmd --zone=public --add-port=10087-12086/tcp --permanent
+firewall-cmd --zone=public --add-port=20086/tcp --permanent
+firewall-cmd --zone=public --add-port=20087/tcp --permanent
+firewall-cmd --zone=public --add-port=20087/udp --permanent
 firewall-cmd --reload
 
+echo "---------------------安装BBR加速------------------------"
 wget –no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
 chmod +x bbr.sh
 ./bbr.sh
